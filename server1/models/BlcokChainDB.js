abf40f15c142b1b660da8492cf8798131a88b1c2
LeessangMin <esm2103@gmail.com>
2022-01-14 17:30:26 +0900

    된다 이제 되는거다! DB에서 제이슨형식의 블록 객체형태로 변환해서 배열에 담는부분 수정

---
+++ server1/models/BlcokChainDB.js
@@ -0,0 +1,33 @@
+const Sequelize = require("sequelize");
+
+module.exports = class BlcokChainDB extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        BCNUM: {
+          type: Sequelize.INTEGER.UNSIGNED,
+          primaryKey: true,
+          allowNull: false,
+          autoIncrement: true,
+        },
+
+        BlockChain: {
+          type: Sequelize.JSON,
+          allowNull: true,
+        },
+      },
+      {
+        sequelize,
+        timestamps: false,
+        underscored: false,
+        modelName: "BC1",
+        tableName: "BC1",
+        paranoid: false,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+
+  static associate(db) {}
+};
